2e372091401d986470d809c9fc9da0a13d294d88
s29385 <s29385@pjwstk.edu.pl>
2024-04-14 14:47:47 +0200

    wersja3

---
+++ WebApplication1/WebApplication1/Animals/ConfigurationForAnilmas.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Animals;
+
+public static class ConfigurationForAnilmas
+{
+    public static void RegisterEndpointsForAnimals(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/animals", (IAnimalsService service) => service.GetAnimals());
+        app.MapGet("/animals/{id}", (int id, IAnimalsService service) => 
+            service.GetAnimalById(id) is Animal animal ? Results.Ok(animal) : Results.NotFound());
+        app.MapPost("/animals", (Animal animal, IAnimalsService service) => 
+            Results.Created($"/animals/{animal.Id}", service.AddAnimal(animal)));
+        app.MapPut("/animals/{id}", (int id, Animal updateAnimal, IAnimalsService service) => 
+            service.UpdateAnimal(id, updateAnimal) is Animal animal ? Results.Ok(animal) : Results.NotFound());
+        app.MapDelete("/animals/{id}", (int id, IAnimalsService service) => {
+            service.DeleteAnimal(id);
+            return Results.Ok();
+        });
+    }
+}
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -16,21 +16,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/animals", (IAnimalsService service) => service.GetAnimals());
-app.MapGet("/animals/{id}", (int id, IAnimalsService service) => 
-    service.GetAnimalById(id) is Animal animal ? Results.Ok(animal) : Results.NotFound());
-app.MapPost("/animals", (Animal animal, IAnimalsService service) => 
-    Results.Created($"/animals/{animal.Id}", service.AddAnimal(animal)));
-app.MapPut("/animals/{id}", (int id, Animal updateAnimal, IAnimalsService service) => 
-    service.UpdateAnimal(id, updateAnimal) is Animal animal ? Results.Ok(animal) : Results.NotFound());
-app.MapDelete("/animals/{id}", (int id, IAnimalsService service) => {
-    service.DeleteAnimal(id);
-    return Results.Ok();
-});
-app.MapGet("/animals/{animalId}/visits", (int animalId, IVisitsService service) => 
-    service.GetVisitsForAnimal(animalId));
-app.MapPost("/visits", (Visit visit, IVisitsService service) => 
-    Results.Created($"/visits/{visit.Id}", service.AddVisit(visit)));
+app.RegisterEndpointsForAnimals();
+app.RegisterEndpointsForVisits();
+
 
 app.UseHttpsRedirection();
 
