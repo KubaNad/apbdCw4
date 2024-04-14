b02604a58ea481f0990f35eac13da532272ff069
s29385 <s29385@pjwstk.edu.pl>
2024-04-14 23:24:58 +0200

    ost proba

--- WebApplication1/WebApplication1/Animals/ConfigurationForAnilmas.cs
+++ WebApplication1/WebApplication1/Animals/ConfigurationForAnilmas.cs
@@ -15,5 +15,50 @@ public static class ConfigurationForAnilmas
             service.DeleteAnimal(id);
             return Results.Ok();
         });
+
+        // app.MapGet("/api/animals", (IAnimalsService service) => Results.Ok(service.GetAnimals()))
+        //     .WithName("GetAnimals")
+        //     .WithOpenApi();
+        //
+        // app.MapGet("/api/animals/{id:int}", (int id, IAnimalsService service) => 
+        //     {
+        //         var animal = service.GetAnimalById(id);
+        //         return animal == null ? Results.NotFound($"Animal with id {id} was not found") : Results.Ok(animal);
+        //     })
+        //     .WithName("GetAnimal")
+        //     .WithOpenApi();
+        //
+        // app.MapPost("/api/animals", (Animal animal, IAnimalsService service) => 
+        //     {
+        //         service.AddAnimal(animal);
+        //         return Results.Created($"/api/animals/{animal.Id}", animal);
+        //     })
+        //     .WithName("CreateAnimal")
+        //     .WithOpenApi();
+        //
+        // app.MapPut("/api/animals/{id:int}", (int id, Animal updateAnimal, IAnimalsService service) => 
+        //     {
+        //         var animalToUpdate = service.GetAnimalById(id);
+        //         if (animalToUpdate == null)
+        //         {
+        //             return Results.NotFound($"Animal with id {id} was not found");
+        //         }
+        //         service.UpdateAnimal(id, updateAnimal);
+        //         return Results.Ok(updateAnimal);
+        //     })
+        //     .WithName("UpdateAnimal")
+        //     .WithOpenApi();
+        //
+        // app.MapDelete("/api/animals/{id:int}", (int id, IAnimalsService service) => 
+        //     {
+        //         if (service.GetAnimalById(id) == null)
+        //         {
+        //             return Results.NotFound($"Animal with id {id} was not found");
+        //         }
+        //         service.DeleteAnimal(id);
+        //         return Results.NoContent();
+        //     })
+        //     .WithName("DeleteAnimal")
+        //     .WithOpenApi();
     }
 }
